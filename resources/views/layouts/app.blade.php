3f9fe115120aff223726dca411ba9bee159adf87
unknown <afsc.analysis@gmail.com>
2019-05-13 17:41:50 -0400

    prueba3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Prueba
+                    Prueba BRANCH mm
                 </a>
             </div>
 
